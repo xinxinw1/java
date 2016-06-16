ce0c521d06353dfa2998006bee692e9c236df28c
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-06-16 17:06:46 -0400

    abstract and interface

--- ECE150Practice/Test.java
+++ ECE150Practice/Test.java
@@ -4,47 +4,37 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args) throws NewException {
-    $.prn(test2());
-  }
-  
-  public static int test(){
-    try {
-      if (1 == 1){
-        throw new NewException("test");
-      } else if (1 == 2) {
-        throw new NewException2("test");
-      } else if (6 == 3){
-        throw new Exception("test");
+    Interf p = new Parent() {
+      public void test(){
+        $.prn("here");
       }
-      return 1;
-    } catch (NewException e){
-      $.prn(e);
-      return 3;
-    } catch (NewException2 e){
-      $.prn(e);
-      return 3;
-    } catch (Exception e){
-      $.prn(e);
-      return 3;
-    } finally {
-      $.prn("here");
-      //return 5;
-    }
+      
+      public int whoa(){
+        return 3;
+      }
+    };
+    
+    p.test();
+    
+    $.prn(((Parent) p).whoa);
+    ((Parent) p).whoa = 66;
+    $.prn(((Parent) p).whoa);
+    $.prn(((Parent) p).hey);
+  }
+}
+
+abstract class Parent implements Interf {
+  public int whoa = 3;
+  static public final int hey = 4;
+
+  public Parent(){
+    $.prn("here");
   }
   
-  public static int test2() throws NewException {
-    throw new NewException("hey");
-  }
+  public abstract void test();
 }
 
-class NewException extends Exception {
-  public NewException(String message){
-    super(message);
-  }
-}
-
-class NewException2 extends Exception {
-  public NewException2(String message){
-    super(message);
-  }
+interface Interf {
+  public void test();
+  public int whoa();
 }
