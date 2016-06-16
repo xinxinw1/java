66a01c4b99da90f68f66aef7c6aaf8e986ad7284
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-06-16 15:47:32 -0400

    practice Exceptions

--- ECE150Practice/Test.java
+++ ECE150Practice/Test.java
@@ -4,37 +4,32 @@ import java.util.*;
 
 public class Test {
   public static void main(String[] args){
-    ArrayList<Integer> a = new ArrayList<>();
-    a.add(3);
-    a.add(5);
-    for (int i = 0; i < 20; i++){
-      a.add(i);
+    try {
+      if (1 == 2){
+        throw new NewException("test");
+      } else if (1 == 2) {
+        throw new NewException2("test");
+      } else {
+        throw new Exception("test");
+      }
+    } catch (NewException e){
+      $.prn(e);
+    } catch (NewException2 e){
+      $.prn(e);
+    } catch (Exception e){
+      $.prn(e);
     }
-    a.get(0);
-    $.prn(a.size());
   }
 }
 
-class Parent {
-  public Parent(){
-    $.prn("creating parent");
-  }
-  
-  public void test(){
-    $.prn("Hey!");
+class NewException extends Exception {
+  public NewException(String message){
+    super(message);
   }
 }
 
-class Child extends Parent {
-  public Child(){
-    super();
-    $.prn("creating child");
-  }
-  
-  public void test(){
-    $.prn("Whoa!");
-    super.test();
+class NewException2 extends Exception {
+  public NewException2(String message){
+    super(message);
   }
 }
-  
-  
