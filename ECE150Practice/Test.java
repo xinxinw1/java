49b4a2e7f54ffee81da01682161b85aeafdaa300
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-06-16 17:17:25 -0400

    non static inner class

--- ECE150Practice/Test.java
+++ ECE150Practice/Test.java
@@ -3,9 +3,13 @@ import tools.$;
 import java.util.*;
 
 public class Test {
-  static int i = 0;
+  int i = 0;
   
   public static void main(String[] args) {
+    new Test().main2();
+  }
+  
+  public void main2(){
     Interf a = new Interf(){
       public void test(){
         i++;
